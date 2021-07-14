7174d71c810cd0888cc86d87ae628e1c7fdb1c06
Luke Otwell <lukehenryotwell@gmail.com>
2021-07-14 18:53:04 -0400

    commit tree 045bdf4423e3fc71c4592ef55472d93a46a65d1c

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function createEmployeeRecord(x) {
+
+    // // firstName = x[0].split(' ')[0]
+    // let firstName = x[0]
+    // let familyName = x[1]
+    // let title = x[2]
+    // let payPerHour = x[3]
+
+    let testEmployee = { firstName: x[0], familyName: x[1], title: x[2], payPerHour: x[3] }
+    testEmployee.timeInEvents = []
+    testEmployee.timeOutEvents = []
+
+    console.log(testEmployee)
+
+    return testEmployee
+}
+
+
+// function createEmployeeRecords(x) {
+
+// //     let employeeObjects = x.forEach(createEmployeeRecord)
+// //     employeeRecords = []
+// //     employeeRecords.push(employeeObjects)
+// //     console.log(employeeRecords)
+
+// // }
+
+
+// const employeeArray = ["Gray", "Worm", "Security", 1]
+
+
+
+
+
+
+
+let twoRows = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+]
+
+
+createEmployeeRecords(twoRows)
